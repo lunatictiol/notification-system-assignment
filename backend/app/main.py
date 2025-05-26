e863b58592a51c2e8cd16b85cac5d597867a854a
lunatictiol <bhatsabiq9@gmail.com>
2025-05-26 12:05:36 +0530

    (fix):comments and worker fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routes import device,notification
 from app.db import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
-
+#main entery point of the app 
 app = FastAPI()
 
 # allow Nginx-served frontend (localhost:8080)
@@ -13,12 +13,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+# This event runs when the FastAPI app starts.
+# It initializes the database by creating all tables defined in SQLAlchemy models.
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+#register different routes
 app.include_router(device.router)
 app.include_router(notification.router)
 
